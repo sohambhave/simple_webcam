cd1e8657c36df1fe5ad42f7c484b157ed20f79fd
sohambhave <sohambhave1998@gmail.com>
2020-06-01 12:10:02 +0530

    Created a image publisher
    Used cv2 to capture video stream.
    Used cv_bridge to convert between CV Image and ROS Image

--- scripts/video_pub.py
+++ scripts/video_pub.py
@@ -3,18 +3,25 @@
 import rospy
 import numpy as np
 import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) #Video capture object for first camera
+bridge = CvBridge()
 
 def video_pub():
-	#pub = rospy.Publisher('video_pub', String, queue_size=10)
+	video_pub = rospy.Publisher('video_pub', Image, queue_size = 10) # Publisher with message type sensor_msgs.msg/Image
 	rospy.init_node('video_pub', anonymous=True)
-	#rate = rospy.Rate(10) # 10hz
-	#while not rospy.is_shutdown():
-	while True:
-		ret, frame = cap.read()
-		cv2.imshow('frame',gray)
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
+	rospy.loginfo("Starting Webcam")
+	while not rospy.is_shutdown():
+		ret, cv_image = cap.read()
+		Image_msg = bridge.cv2_to_imgmsg(cv_image, "bgr8")
+		video_pub.publish(Image_msg)
+		cv2.imshow('Camera 1',cv_image)
+		cv2.waitKey(1)
+		
+if __name__ == '__main__':
+	video_pub()
+	rospy.loginfo("Closing Webcam")
 	cap.release()
 	cv2.destroyAllWindows()
