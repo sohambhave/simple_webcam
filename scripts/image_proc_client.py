877c2d0fdd738db6d96dd8d9f48b173a82db8489
sohambhave <sohambhave1998@gmail.com>
2020-06-01 19:10:46 +0530

    Modified service not to take image argument

--- scripts/image_proc_client.py
+++ scripts/image_proc_client.py
@@ -10,34 +10,34 @@ from simple_webcam.srv import Image_Proc,Image_ProcResponse
 
 bridge = CvBridge()
 
-def image_proc_client(Img,BLUR,CROP,crop_x, crop_y, crop_H, crop_W):
+def image_proc_client(BLUR,CROP,crop_x, crop_y, crop_H, crop_W):
 	rospy.wait_for_service('image_proc')
 	try:
 		image_proc = rospy.ServiceProxy('image_proc', Image_Proc)
-		resp1 = image_proc(Img,BLUR,CROP,crop_x, crop_y, crop_H, crop_W)
+		resp1 = image_proc(BLUR,CROP,crop_x, crop_y, crop_H, crop_W)
 		cv_image_out = bridge.imgmsg_to_cv2(resp1.image_out, "bgr8")
 		return cv_image_out
 	except rospy.ServiceException as e:
 		print("Service call failed: %s"%e)
 
-def video_callback(Image_msg):
-	cv_image = bridge.imgmsg_to_cv2(Image_msg, "bgr8")
-	cv2.imshow('Camera 1 Raw',cv_image)
-	blur_img = image_proc_client(Image_msg, True, False, 0, 0, 0, 0)
+def usage():
+	blur_img = image_proc_client(True, False, 0, 0, 0, 0)
 	cv2.imshow('Camera 1 Blur',blur_img)
-	crop_img = image_proc_client(Image_msg, False, True, 200, 100, 200, 200)
+	crop_img = image_proc_client(False, True, 200, 100, 200, 200)
 	cv2.imshow('Camera 1 Crop',crop_img)
 	cv2.waitKey(1)
 	
 
 def video_sub():
 	rospy.init_node('image_proc_client', anonymous=True)
-	rospy.Subscriber("video_stream", Image, video_callback)
 	rospy.loginfo("Started Image Processing Client")
-	rospy.spin()	
 		
 if __name__ == '__main__':
 	video_sub()
+	rate = rospy.Rate(10) # 10hz
+	while not rospy.is_shutdown():
+		usage()
+		rate.sleep()
 	rospy.loginfo("Closing subscriber")
 	cv2.destroyAllWindows()
 	
--- scripts/image_proc_server.py
+++ scripts/image_proc_server.py
@@ -6,14 +6,21 @@ import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from simple_webcam.srv import Image_Proc,Image_ProcResponse
-#import simple_webcam.srv
 
 bridge = CvBridge()
 
+class my_img:
+	def __init__(self):
+		self.img = bridge.cv2_to_imgmsg(np.zeros((500,500,3), np.uint8), "bgr8")
+	def get_img(self):
+		return(self.img)
+	def set_img(self,img):
+		self.img = img
+
+I = my_img()
+
 def image_proc_handle(req):
-	img = req.image_in
-	img = bridge.imgmsg_to_cv2(img, "bgr8")
-	
+	img = bridge.imgmsg_to_cv2(I.get_img(), "bgr8")
 	if req.BLUR == True:
 		img = cv2.blur(img,(10,10))
 	
@@ -23,10 +30,13 @@ def image_proc_handle(req):
 	img = bridge.cv2_to_imgmsg(img, "bgr8")
 	return(Image_ProcResponse(img))
 
-
+def video_callback(Image_msg):
+	I.set_img(Image_msg)
+	
 def image_proc_server():
 	rospy.init_node('image_proc_server', anonymous=True)
 	rospy.loginfo("Starting Image Processing Server")
+	rospy.Subscriber("video_stream", Image, video_callback)
 	s = rospy.Service('image_proc', Image_Proc, image_proc_handle)
 	rospy.spin()
 	
